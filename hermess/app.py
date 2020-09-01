7b17d7b723fcaf41b07dd23a1ab23f78533de793
YongSa <61119786+YongSa@users.noreply.github.com>
2020-09-01 15:15:47 +0200

    Add files via upload
    added widget function for plot

--- hermess/app.py
+++ hermess/app.py
@@ -2,41 +2,22 @@ import sys
 import time
 from hermess import log, plot
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from tabulate import tabulate
+import matplotlib
+matplotlib.use('Qt5Agg')
 
-"""
-ToDo
-    - offset (38, 76), startadresse auf GUI eingeben
-    - lb sowie hb eingebbar (station address), ID für einzelne brücken sowie für insgesamt (667)
-    - speichern von confi
-"""
-#floatfmt=".16f"; zur Zeit floats abgeschnitten
-#was schnelleres als tabulate benutzen (strings/list to csv)
-#Graphisch Darstellen?
-#convert nimmt auch nur Format 64 LEERZEILE 08, kein 6408, kein 64,08, was wenn 5 werte rauskommen -> Fehler!
-#dmsdat[i/2] = ddat[i/2]*messber[i/2]/0.065536/((1.0+nue[i/2])/4*kfakt[i/2])/uspeise[i/2];
-"""
-if (read_adc > 0) {
-    adc_fd = open("/sys/bus/iio/devices/iio:device0/in_voltage1_raw", O_RDONLY);
-    read(adc_fd, &adcval, 5);
-    fadcval = (atof(adcval)-adc_zero)*1.8/4095.0*adc_cal;
-    close(adc_fd);
-    0,1575 = (2639 - 2615)*1,8/4095*14,927 -> 2639mV ... 2,639V
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 
-    for (i=0; i<8; i++) {
-       kfakt[i]   = 2.0;
-       uspeise[i] = 2.5;
-       messber[i] = .005;
-       nue[i]     = 0.3;
-          fprintf(stderr,"\nMessbereich:         ");
-for (i=0; i<8; i++) {fprintf(stderr,"%8.4f",messber[i]); messber[i] = 2.*messber[i];}
-fprintf(stderr,"\nSpeisespannung:      ");
-for (i=0; i<8; i++) {fprintf(stderr,"%8.4f",uspeise[i]);}
-fprintf(stderr,"\nk-Faktor DMS:        ");
-for (i=0; i<8; i++) {fprintf(stderr,"%8.4f",kfakt[i]);}
-fprintf(stderr,"\nQuerkontraktion nue: ");
-for (i=0; i<8; i++) {fprintf(stderr,"%8.4f",nue[i]);}
-"""
+import pandas as pd
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
 qtCreatorFile = "mainwindow.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
@@ -48,11 +29,32 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.startButton.clicked.connect(self.start_logging)
         self.stopButton.clicked.connect(self.stop_logging)
         self.saveButton.clicked.connect(self.save_results)
-        self.testPlotButton.clicked.connect(self.test_plot)
-        self.setSR_DMSButton.clicked.connect(self.setSR_DMS)
-        self.setSR_TEMPButton.clicked.connect(self.setSR_TEMP)
-        self.setFF_DMSButton.clicked.connect(self.setFF_DMS)
-        self.tareButton.clicked.connect(self.tare)
+
+        # Create the maptlotlib FigureCanvas object,
+        # which defines a single set of axes as self.axes.
+        sc = MplCanvas(self, width=5, height=4, dpi=100)
+
+        # Create our pandas DataFrame with some simple
+        # data and headers.
+        df = pd.DataFrame([
+           [0, 10], [5, 15], [2, 20], [15, 25], [4, 10],
+        ], columns=['A', 'B'])
+
+        # plot the pandas DataFrame, passing in the
+        # matplotlib Canvas axes.
+        df.plot(ax=sc.axes)
+
+        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
+        toolbar = NavigationToolbar(sc, self)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(toolbar)
+        layout.addWidget(sc)
+
+        # Create a placeholder widget to hold our toolbar and canvas.
+        self.plotWidget.setLayout(layout)
+        #self.setCentralWidget(widget)
+        self.plotWidget.show()
 
     def start_logging(self):
         log.setup(self)
@@ -61,29 +63,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         log.stop()
 
     def save_results(self):
-        try:
-            save = open("log/" + time.strftime("%Y%m%d-%H%M%S") + ".txt","w")
-            save.write(tabulate(log.getTable(), headers=["TIME","CAN_ID", "CAN_DATA", "1", "2", "3", "4"], tablefmt="tsv", missingval="?"))
-            save.close()
-            log.emptyTable()
-        except:
-            print("Wasn't able to save!")
-
-    def test_plot(self):
-        plot.plotTest("test3.txt")
-
-    def setSR_DMS(self):
-        log.setSRFF(0x667, 0xEA, 0x0E, 0x26, self.dmsSRCombo.currentIndex()) #Index 0 stimmt eventuell nicht, kein 1Hz
-
-    def setSR_TEMP(self):
-        log.setSRFF(0x667, 0xA2, 0x0C, 0x26, self.tempSRCombo.currentIndex())#Index 0 stimmt eventuell nicht, kein 1Hz
-
-    def setFF_DMS(self):
-        log.setSRFF(0x667, 0xEA, 0x0E, 0x28, ((self.dmsFFCombo.currentIndex()+1*self.dmsFFCombo.currentIndex()+1)*2)) #Index anscheinend kein Indikator (-> 1 2, 2 8, 3 18 ...) für Filter*2
-
-    def tare(self):
-        log.tare(0x667,0xEA,0x0E)
-        log.tare(0x667,0xA2,0x0C)
+        pass
 
     def closeEvent(self, event):
         if not log.getTable():
